47aa755e6dc6b4a161830cb8c253277dd3ebd751
Jan Graichen <jgraichen@altimos.de>
2025-04-20 20:52:37 +0200

    feat: Someline36 cutouts for pin in lid

--- someline-36.py
+++ someline-36.py
@@ -12,6 +12,12 @@ HEIGHT = 33.7
 INNER_ROW_SIZE = 24.5
 OUTER_ROW_SIZE = 24.0
 
+LIT_PIN_DY = 3.8
+LIT_PIN_LENGTH = 4.0
+LIT_PIN_TOPW = 3.0
+LIT_PIN_BOTW = 2.0
+LIT_PIN_DEPTH = 5.0
+
 
 def unit_to_length(u: int):
     if u < 3:
@@ -57,6 +63,34 @@ def make(units: int):
                     b.add(padM)
                     b.add(pocketM, mode=b.Mode.SUBTRACT)
 
+        with b.BuildPart(mode=b.Mode.PRIVATE) as cutout:
+            with b.BuildSketch(b.Plane.YZ):
+                with b.BuildLine():
+                    b.Polyline(
+                        [
+                            (-(LIT_PIN_BOTW / 2), -LIT_PIN_DEPTH),
+                            (-(LIT_PIN_TOPW / 2), 0.0),
+                            (+(LIT_PIN_TOPW / 2), 0.0),
+                            (+(LIT_PIN_BOTW / 2), -LIT_PIN_DEPTH),
+                            (-(LIT_PIN_BOTW / 2), -LIT_PIN_DEPTH),
+                        ]
+                    )
+                b.make_face()
+            b.extrude(amount=LIT_PIN_LENGTH)
+            b.fillet(
+                cutout.edges().group_by(b.Axis.Z)[0].filter_by(b.Axis.X),
+                radius=(LIT_PIN_BOTW / 3),
+            )
+
+        with b.Locations((0.0, LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
+        with b.Locations((0.0, WIDTH - LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
+        with b.Locations((length - LIT_PIN_LENGTH, LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
+        with b.Locations((length - LIT_PIN_LENGTH, WIDTH - LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
+
     return part.part
 
 
@@ -91,6 +125,26 @@ def u5():
 @project.model("Someline-36-U6", color=b.Color(0xFF6A13))
 def u6():
     return make(units=6)
+
+
+@project.model("Someline-36-U7", color=b.Color(0xFF6A13))
+def u7():
+    return make(units=7)
+
+
+@project.model("Someline-36-U8", color=b.Color(0xFF6A13))
+def u8():
+    return make(units=8)
+
+
+@project.model("Someline-36-U9", color=b.Color(0xFF6A13))
+def u9():
+    return make(units=9)
+
+
+@project.model("Someline-36-U10", color=b.Color(0xFF6A13))
+def u10():
+    return make(units=10)
 
 
 if __name__ == "__main__":
