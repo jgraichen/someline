81f3304735a74022757ab6fabaf10a739c54e621
Jan Graichen <jgraichen@altimos.de>
2025-05-04 13:48:36 +0200

    feat(36): Redesign sizes and box geometry
    * Use a loft box design, which is ~.5mm smaller at the bottom that on
      the top, which accounts better for perfectly vertical walls and
      tolerances on the Someline box and wall brackets.
    * The U1 box has additional pocket cutouts to account for the thinner
      row size on the outer pockets in the boxes. The wall brackets would
      collide with 7mm rounded edges otherwise.
    * Equally size all row compartments: All boxes use the same row sizing
      now, there is no difference in INNER and OUTER row size anymore.
      Therefore, no original walls inserts are needed anymore to fill in
      gaps when using many smaller insets.

--- someline-36.py
+++ someline-36.py
@@ -3,14 +3,19 @@
 
 import build123d as b
 
-from someline.someline import make_box, make_handle, make_wall_cutout
+from someline.someline import (
+    make_handle,
+    make_loft_box,
+    make_wall_cutout,
+    make_wall_cutout_pocket,
+)
 from someline.util import Project
 
 WIDTH = 41.2
 HEIGHT = 33.7
 
-INNER_ROW_SIZE = 24.5
-OUTER_ROW_SIZE = 24.0
+INNER_ROW_SIZE = 24.4  # 24.5
+OUTER_ROW_SIZE = 24.4  # 24.0
 
 WALL_DEPTH = 1.6
 
@@ -28,7 +33,16 @@ def unit_to_length(u: int):
         return 2 * OUTER_ROW_SIZE + (u - 2) * INNER_ROW_SIZE
 
 
-def b_cap_hinge_cutout(length):
+def make_s36_cutouts():
+    return make_wall_cutout(
+        outer_width=9.0,
+        inner_width=6.0,
+        depth=3.0,
+        height=24.1,
+    )
+
+
+def b_cap_hinge_cutout(length, half=False):
     with b.BuildPart(mode=b.Mode.PRIVATE) as cutout:
         with b.BuildSketch(b.Plane.YZ):
             with b.BuildLine():
@@ -52,32 +66,27 @@ def b_cap_hinge_cutout(length):
         b.add(cutout, mode=b.Mode.SUBTRACT)
     with b.Locations((0.0, WIDTH - LIT_PIN_DY, HEIGHT)):
         b.add(cutout, mode=b.Mode.SUBTRACT)
-    with b.Locations((length - LIT_PIN_LENGTH, LIT_PIN_DY, HEIGHT)):
-        b.add(cutout, mode=b.Mode.SUBTRACT)
-    with b.Locations((length - LIT_PIN_LENGTH, WIDTH - LIT_PIN_DY, HEIGHT)):
-        b.add(cutout, mode=b.Mode.SUBTRACT)
+    if not half:
+        with b.Locations((length - LIT_PIN_LENGTH, LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
+        with b.Locations((length - LIT_PIN_LENGTH, WIDTH - LIT_PIN_DY, HEIGHT)):
+            b.add(cutout, mode=b.Mode.SUBTRACT)
 
 
 def make(units: int):
     length = unit_to_length(units)
 
     with b.BuildPart() as part:
-        with make_box(length, WIDTH, HEIGHT, wall_depth=WALL_DEPTH) as box:
+        with make_loft_box(length, WIDTH, HEIGHT, wall_depth=WALL_DEPTH) as box:
             with b.Locations((0.0, WIDTH, HEIGHT)):
                 if units < 3:
                     handle = make_handle(length=length, thickness=1.2)
                 else:
-                    handle = make_handle(length=40, thickness=1.2)
+                    handle = make_handle(length=50, thickness=1.2)
                 b.add(handle)
 
             if units > 1:
-                pad, pocket = make_wall_cutout(
-                    outer_width=9.0,
-                    inner_width=6.0,
-                    depth=3.0,
-                    height=24.1,
-                )
-
+                pad, pocket = make_s36_cutouts()
                 sym_plane = b.Plane.XZ.offset(-WIDTH / 2)
                 with b.BuildPart(mode=b.Mode.PRIVATE) as padM:
                     b.add(pad)
@@ -96,6 +105,26 @@ def make(units: int):
         b.add(box)
         b_cap_hinge_cutout(length)
 
+        # The smallest U1 insets need additional cutouts as they do not
+        # have enough tolerance with two 7mm rounded edges on the wall
+        # separators in the Someline box.
+        if units == 1:
+            pocket = make_wall_cutout_pocket(
+                outer_width=8.0,
+                inner_width=5.0,
+                depth=3.0,
+                height=24.1,
+            )
+
+            with b.GridLocations(OUTER_ROW_SIZE * units, 0, 2, 1, align=b.Align.MIN):
+                b.add(pocket, mode=b.Mode.SUBTRACT)
+
+            with b.Locations((0.0, WIDTH, 0.0)):
+                with b.GridLocations(
+                    OUTER_ROW_SIZE * units, 0, 2, 1, align=b.Align.MIN
+                ):
+                    b.add(pocket, mode=b.Mode.SUBTRACT, rotation=(0.0, 0.0, 180.0))
+
     return part.part
 
 
@@ -103,6 +132,9 @@ def make_cutout_box(units: int):
     length = unit_to_length(units)
 
     with b.BuildPart(mode=b.Mode.PRIVATE) as part:
+        # Sketch the base layout of the inset with a cutout on the front
+        # side for where the lid hinges are in the first row. The sketch
+        # already has rounded corners.
         with b.BuildSketch(b.Plane.XY) as sk:
             with b.BuildLine():
                 b.Polyline(
@@ -133,66 +165,132 @@ def make_cutout_box(units: int):
             b.fillet(vtouter, radius=7)
             b.fillet(vtinner, radius=2.5)
 
-        b.extrude(amount=HEIGHT)
+        # Create a standard loft box based on the customized sketch. The
+        # box generate will ensure the sketch is outlined into a loft
+        # inset box.
+        with make_loft_box(
+            length,
+            WIDTH,
+            HEIGHT,
+            wall_depth=WALL_DEPTH,
+            sketch=sk.sketch,
+        ) as box:
+            with b.Locations((0.0, WIDTH, HEIGHT)):
+                if units <= 3:
+                    b.add(make_handle(length=length, thickness=1.2))
+                else:
+                    b.add(make_handle(length=50, thickness=1.2))
+                b.extrude(sk.sketch, amount=HEIGHT, mode=b.Mode.INTERSECT)
 
-        with b.BuildSketch(b.Plane.XY.offset(1.0)):
-            b.add(sk)
-            b.offset(amount=-WALL_DEPTH, kind=b.Kind.INTERSECTION)
+            pad, pocket = make_s36_cutouts()
 
-        b.extrude(amount=HEIGHT, mode=b.Mode.SUBTRACT)
+            with b.Locations((OUTER_ROW_SIZE, WIDTH, 0.0)):
+                locs = b.GridLocations(
+                    INNER_ROW_SIZE, 0, units - 1, 1, align=b.Align.MIN
+                )
 
-        with b.Locations((0.0, WIDTH, HEIGHT)):
-            if units <= 3:
-                b.add(make_handle(length=length, thickness=1.2))
-            else:
-                b.add(make_handle(length=40, thickness=1.2))
-            b.extrude(sk.sketch, amount=HEIGHT, mode=b.Mode.INTERSECT)
+            for loc in locs:
+                with b.Locations(loc):
+                    # Always add wall cutouts on back side
+                    b.add(pad, rotation=(0, 0, 180.0))
+                    b.add(pocket, mode=b.Mode.SUBTRACT, rotation=(0, 0, 180.0))
 
-        # Round inner edges
-        b.fillet(part.edges().group_by(b.Axis.Z)[1], radius=4)
+                    # Add wall cutouts on front side only if they are not in
+                    # the cutout area for the cap hinges:
+                    if loc.position.X < (length / 2 - 15) or loc.position.X > (
+                        length / 2 + 15
+                    ):
+                        with b.Locations((0.0, -WIDTH, 0.0)):
+                            b.add(pad)
+                            b.add(pocket, mode=b.Mode.SUBTRACT)
 
-        # Bottom edge chamfer
-        b.chamfer(part.edges().group_by(b.Axis.Z)[0], length=1)
+        b.add(box.part)
+        b_cap_hinge_cutout(length)
 
-        # Inner top chamfer
-        b.chamfer(
-            part.edges().group_by(b.Axis.Z)[-1].group_by(b.Axis.Y)[2],
-            length=(WALL_DEPTH - 0.4),
-        )
+    return part.part
 
-        pad, pocket = make_wall_cutout(
-            outer_width=9.0,
-            inner_width=6.0,
-            depth=3.0,
-            height=24.1,
-        )
 
-        with b.Locations((OUTER_ROW_SIZE, WIDTH, 0.0)):
-            locs = b.GridLocations(INNER_ROW_SIZE, 0, units - 1, 1, align=b.Align.MIN)
+def make_half_cutout_box(units: int, flip=False):
+    length = unit_to_length(units) + (INNER_ROW_SIZE / 2)
 
-        for loc in locs:
-            with b.Locations(loc):
-                # Always add wall cutouts on back side
-                b.add(pad, rotation=(0, 0, 180.0))
-                b.add(pocket, mode=b.Mode.SUBTRACT, rotation=(0, 0, 180.0))
+    with b.BuildPart(mode=b.Mode.PRIVATE) as part:
+        # Sketch the base layout of the inset with a cutout on the front
+        # side for where the lid hinges are in the first row. The sketch
+        # already has rounded corners.
+        with b.BuildSketch(b.Plane.XY) as sk:
+            with b.BuildLine():
+                b.Polyline(
+                    [
+                        (0, 0),
+                        (0, WIDTH),
+                        (length, WIDTH),
+                        (length, 10),
+                        (length - 20, 10),
+                        (length - 20, 0),
+                        (0, 0),
+                    ]
+                )
+            b.make_face()
 
-                # Add wall cutouts on front side only if they are not in
-                # the cutout area for the cap hinges:
-                if loc.position.X < (length / 2 - 15) or loc.position.X > (
-                    length / 2 + 15
-                ):
-                    with b.Locations((0.0, -WIDTH, 0.0)):
+            vtouter = (
+                sk.vertices().group_by(b.Axis.X)[0]
+                + sk.vertices().group_by(b.Axis.X)[-1]
+            )
+
+            vtinner = (
+                sk.vertices().group_by(b.Axis.X)[1]
+                + sk.vertices().group_by(b.Axis.X)[2]
+            )
+
+            b.fillet(vtouter, radius=7)
+            b.fillet(vtinner, radius=2.5)
+
+        # Create a standard loft box based on the customized sketch. The
+        # box generate will ensure the sketch is outlined into a loft
+        # inset box.
+        with make_loft_box(
+            length,
+            WIDTH,
+            HEIGHT,
+            wall_depth=WALL_DEPTH,
+            sketch=sk.sketch,
+        ) as box:
+            with b.Locations((0.0, WIDTH, HEIGHT)):
+                if units <= 3:
+                    b.add(make_handle(length=length, thickness=1.2))
+                else:
+                    b.add(make_handle(length=50, thickness=1.2))
+                b.extrude(sk.sketch, amount=HEIGHT, mode=b.Mode.INTERSECT)
+
+            pad, pocket = make_s36_cutouts()
+
+            with b.Locations((OUTER_ROW_SIZE, WIDTH, 0.0)):
+                with b.GridLocations(INNER_ROW_SIZE, 0, units, 1, align=b.Align.MIN):
+                    # Always add wall cutouts on back side
+                    b.add(pad, rotation=(0, 0, 180.0))
+                    b.add(pocket, mode=b.Mode.SUBTRACT, rotation=(0, 0, 180.0))
+
+            if units > 1:
+                # Add one less cutout on front side
+                with b.Locations((OUTER_ROW_SIZE, 0, 0.0)):
+                    with b.GridLocations(
+                        INNER_ROW_SIZE, 0, units - 1, 1, align=b.Align.MIN
+                    ):
                         b.add(pad)
                         b.add(pocket, mode=b.Mode.SUBTRACT)
 
-        b_cap_hinge_cutout(length)
+        b.add(box.part)
+        b_cap_hinge_cutout(length, half=True)
+
+        if flip:
+            b.mirror(about=b.Plane.ZY, mode=b.Mode.REPLACE)
 
     return part.part
 
 
 project = Project("someline-36", default_color=b.Color(0xFF6A13))
 
-for i in range(1, 11):
+for i in range(1, 10):
     project.define(f"Someline-36-U{i}", make, args={"units": i})
 
 
@@ -204,6 +302,26 @@ def c3():
 @project.model("Someline-36-C5")
 def c5():
     return make_cutout_box(units=5)
+
+
+@project.model("Someline-36-A1")
+def a1():
+    return make_half_cutout_box(units=1)
+
+
+@project.model("Someline-36-A2")
+def a2():
+    return make_half_cutout_box(units=2)
+
+
+@project.model("Someline-36-B1")
+def b1():
+    return make_half_cutout_box(units=1, flip=True)
+
+
+@project.model("Someline-36-B2")
+def b2():
+    return make_half_cutout_box(units=2, flip=True)
 
 
 if __name__ == "__main__":
--- someline/someline.py
+++ someline/someline.py
@@ -2,23 +2,33 @@
 
 
 from contextlib import contextmanager
+from typing import Optional
 
 import build123d as b
 
 
 @contextmanager
-def make_box(length: float, width: float, height: float, wall_depth: float = 1.2):
+def make_box(
+    length: float,
+    width: float,
+    height: float,
+    wall_depth: float = 1.2,
+    sketch: Optional[b.Sketch] = None,
+):
     with b.BuildPart(mode=b.Mode.PRIVATE) as part:
         b.Box(length, width, height, align=None)
 
         with b.BuildSketch(b.Plane.XY.offset(1.0)):
-            with b.Locations((wall_depth, wall_depth)):
-                b.RectangleRounded(
-                    length - (2 * wall_depth),
-                    width - (2 * wall_depth),
-                    align=b.Align.MIN,
-                    radius=(7 - wall_depth),
-                )
+            if sketch:
+                b.add(sketch)
+            else:
+                with b.Locations((wall_depth, wall_depth)):
+                    b.RectangleRounded(
+                        length - (2 * wall_depth),
+                        width - (2 * wall_depth),
+                        align=b.Align.MIN,
+                        radius=(7 - wall_depth),
+                    )
 
         b.extrude(amount=height, mode=b.Mode.SUBTRACT)
         b.fillet(part.edges().group_by(b.Axis.Z)[1], radius=4)
@@ -39,6 +49,65 @@ def make_box(length: float, width: float, height: float, wall_depth: float = 1.2
         # Inner top chamfer
         b.chamfer(
             part.edges().group_by(b.Axis.Z)[-1].group_by(b.Axis.Y)[1],
+            length=(wall_depth - 0.4),
+        )
+
+    return part.part
+
+
+@contextmanager
+def make_loft_box(
+    length: float,
+    width: float,
+    height: float,
+    wall_depth: float = 1.2,
+    bottom_depth: float = 1.2,
+    loft: float = 0.5,
+    sketch: Optional[b.Sketch] = None,
+):
+    with b.BuildPart(mode=b.Mode.PRIVATE) as box:
+        with b.BuildSketch(b.Plane.XY.offset(height)) as skt:
+            if sketch:
+                b.add(sketch)
+            else:
+                b.RectangleRounded(
+                    width=length,
+                    height=width,
+                    align=b.Align.MIN,
+                    radius=7,
+                )
+        with b.BuildSketch(b.Plane.XY) as skb:
+            b.add(skt)
+            b.offset(amount=-loft, kind=b.Kind.ARC)
+        b.loft()
+
+    with b.BuildPart(mode=b.Mode.PRIVATE) as part:
+        b.add(box)
+        b.offset(
+            amount=-wall_depth,
+            openings=part.faces().group_by(b.Axis.Z)[-1]
+            + part.faces().group_by(b.Axis.Z)[0],
+        )
+        b.extrude(skb.sketch, amount=bottom_depth)
+
+        # Round inner bottom edges
+        b.fillet(part.edges().group_by(b.Axis.Z)[1], radius=4)
+
+        yield part
+
+        # Cutoff excess stuff
+        b.add(box, mode=b.Mode.INTERSECT)
+
+        # Bottom edge chamfer
+        zy = (
+            part.edges().group_by(b.Axis.Z)[0].group_by(b.Axis.Y)[0]
+            + part.edges().group_by(b.Axis.Z)[0].group_by(b.Axis.Y)[-1]
+        )
+        b.chamfer(zy, length=1)
+
+        # Inner top chamfer
+        b.chamfer(
+            part.edges().group_by(b.Axis.Z)[-1].group_by(b.Axis.X)[1],
             length=(wall_depth - 0.4),
         )
 
@@ -70,6 +139,39 @@ def make_wall_cutout(
     height: float,
     wall: float = 0.8,
 ):
+    pocket = make_wall_cutout_pocket(
+        outer_width=outer_width,
+        inner_width=inner_width,
+        depth=depth,
+        height=height,
+    )
+
+    with b.BuildPart(mode=b.Mode.PRIVATE) as pad:
+        b.add(pocket)
+
+        front_face = pad.faces().sort_by(b.Axis.Y)[0]
+        bottom_face = pad.faces().sort_by(b.Axis.Z)[0]
+        b.offset(
+            amount=wall, kind=b.Kind.INTERSECTION, openings=[front_face, bottom_face]
+        )
+        b.Box(
+            outer_width + wall * 3,
+            depth + wall * 2,
+            1.0,
+            align=(b.Align.CENTER, b.Align.MIN, b.Align.MIN),
+            mode=b.Mode.SUBTRACT,
+        )
+        b.fillet(pad.edges().filter_by(b.Axis.Z).group_by(b.Axis.Y)[-1], radius=wall)
+
+    return (pad.part, pocket)
+
+
+def make_wall_cutout_pocket(
+    outer_width: float,
+    inner_width: float,
+    depth: float,
+    height: float,
+):
     with b.BuildPart(mode=b.Mode.PRIVATE) as pocket:
         with b.BuildSketch(b.Plane.XY):
             with b.BuildLine():
@@ -90,21 +192,4 @@ def make_wall_cutout(
             length=(depth - 0.001),
         )
 
-    with b.BuildPart(mode=b.Mode.PRIVATE) as pad:
-        b.add(pocket)
-
-        front_face = pad.faces().sort_by(b.Axis.Y)[0]
-        bottom_face = pad.faces().sort_by(b.Axis.Z)[0]
-        b.offset(
-            amount=wall, kind=b.Kind.INTERSECTION, openings=[front_face, bottom_face]
-        )
-        b.Box(
-            outer_width + wall * 3,
-            depth + wall * 2,
-            1.0,
-            align=(b.Align.CENTER, b.Align.MIN, b.Align.MIN),
-            mode=b.Mode.SUBTRACT,
-        )
-        b.fillet(pad.edges().filter_by(b.Axis.Z).group_by(b.Axis.Y)[-1], radius=wall)
-
-    return (pad.part, pocket.part)
+    return pocket.part
